4d68a09fe56b31ac86a651a3e8375839fbacd4fa
pawaanyako <pawaanyako@gmail.com>
2026-08-27 22:42:42 +0300

    update gitignore

--- QRCode.cpp
+++ QRCode.cpp
@@ -5,7 +5,7 @@ void main(int argc, char** argv) {
 	try {
 		InitializeMagick(*argv);
 		setlocale(LC_ALL, "Russian");
-		string _string = "vk.com/pskovsb";
+		string _string = "https://youtu.be/dQw4w9WgXcQ";
 		Bits* _bits = new Bits(_string);
 		Image image = Draw::DrawQRCode(_bits);
 		image.write("QRCode.png");
